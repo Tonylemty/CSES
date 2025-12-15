d8f785fb42265efa365f7e45ce1e1b1077af8db4
Tonylemty <chengshinchung31@gmail.com>
2025-12-15 11:10:41 +0800

    Bit Strings

--- introductory_problems/code/bit_strings.cpp
+++ introductory_problems/code/bit_strings.cpp
@@ -15,11 +15,11 @@ int main() {
 
     // 快速冪 (binary exponentiation)
     while (n > 0) {
-        if (n % 2 == 1) {
+        if (n % 2 == 1) { // 奇數 = 這一位（二進位）是 1，代表這個次方「要用」
             ans = ans * base % M;
         }
         base = base * base % M;
-        n /= 2;
+        n /= 2; // 相當於 n 的二進位右移 1 位
     }
     cout << ans << "\n";
     
