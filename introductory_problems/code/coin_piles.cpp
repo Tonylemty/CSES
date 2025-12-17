cc6853f0ccd8e9cd6880a9d1a899b46e3f8e14bc
Tonylemty <chengshinchung31@gmail.com>
2025-12-18 00:32:30 +0800

    Coin Piles

---
+++ introductory_problems/code/coin_piles.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        ll a, b;
+        cin >> a >> b;
+
+        if (((a + b) % 3 == 0) && max(a, b) <= 2 * min(a, b)) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
+        }
+    }
+
+    return 0;
+}
