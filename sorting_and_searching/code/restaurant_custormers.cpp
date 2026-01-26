f5b793aaa4bc18e709da579e4ba418c0b93c01be
Tonylemty <chengshinchung31@gmail.com>
2026-01-26 11:22:22 +0800

    Restaurant Customers

---
+++ sorting_and_searching/code/restaurant_custormers.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    vector<pair<ll, int>> events(2 * n);
+
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        events.push_back({a, 1});
+        events.push_back({b, -1});
+    }
+    sort(events.begin(), events.end());
+
+    int cur = 0;
+    int ans = 0;
+    for (auto &e : events) {
+        cur += e.second;
+        ans = max(ans, cur);
+    }
+    cout << ans;
+        
+    return 0;
+}
